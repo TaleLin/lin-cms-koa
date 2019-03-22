b1f0aab7946d9b3e0cae458887ca647c98a621c5
pedro <gaopedro@163.com>
2019-03-22 10:32:55 +0800

    update: 重构目录规范，增加dao

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,12 +1,6 @@
 "use strict";
 
-const {
-  LinRouter,
-  NotFound,
-  Success,
-  Forbidden,
-  groupRequired
-} = require("lin-cms");
+const { LinRouter, NotFound, Success, groupRequired } = require("lin-cms");
 const { getSafeParamId } = require("../../libs/util");
 const {
   BookSearchValidator,
@@ -14,7 +8,8 @@ const {
 } = require("../../validators/cms");
 const { BookNotFound } = require("../../libs/err-code");
 const { Book } = require("../../models/book");
-const Sequelize = require("sequelize");
+const { BookDao } = require("../../dao/book");
+
 // book 的红图实例
 const bookApi = new LinRouter({
   prefix: "/v1/book"
@@ -23,89 +18,6 @@ const bookApi = new LinRouter({
 exports.bookApi = bookApi;
 
 // exports[disableLoading] = true;
-
-class BookDao {
-  async getBook (id) {
-    const book = await Book.findOne({
-      where: {
-        id,
-        delete_time: null
-      }
-    });
-    return book;
-  }
-
-  async getBookByKeyword (q) {
-    const book = await Book.findOne({
-      where: {
-        title: {
-          [Sequelize.Op.like]: `%${q}%`
-        },
-        delete_time: null
-      }
-    });
-    return book;
-  }
-
-  async getBooks () {
-    const books = await Book.findAll({
-      where: {
-        delete_time: null
-      }
-    });
-    return books;
-  }
-  async createBook (v) {
-    const book = await Book.findOne({
-      where: {
-        title: v.get("title"),
-        delete_time: null
-      }
-    });
-    if (book) {
-      throw new Forbidden({
-        msg: "图书已存在"
-      });
-    }
-    const bk = new Book();
-    bk.title = v.get("title");
-    bk.author = v.get("author");
-    bk.summary = v.get("summary");
-    bk.image = v.get("image");
-    bk.save();
-  }
-
-  async updateBook (v, id) {
-    const book = await Book.findById(id);
-    if (!book) {
-      throw new NotFound({
-        msg: "没有找到相关书籍"
-      });
-    }
-    book.title = v.get("title");
-    book.author = v.get("author");
-    book.summary = v.get("summary");
-    book.image = v.get("image");
-    book.save();
-  }
-
-  async deleteBook (id) {
-    const book = await Book.findOne({
-      where: {
-        id,
-        delete_time: null
-      }
-    });
-    if (!book) {
-      throw new NotFound({
-        msg: "没有找到相关书籍"
-      });
-    }
-    book.softDelete();
-  }
-}
-
-exports.BookDao = BookDao;
 
 // book 的dao 数据库访问层实例
 const bookDto = new BookDao();
---
+++ app/dao/book.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const { LinRouter, NotFound, Forbidden } = require("lin-cms");
+const { Book } = require("../models/book");
+const Sequelize = require("sequelize");
+// book 的红图实例
+const bookApi = new LinRouter({
+  prefix: "/v1/book"
+});
+
+exports.bookApi = bookApi;
+
+class BookDao {
+  async getBook (id) {
+    const book = await Book.findOne({
+      where: {
+        id,
+        delete_time: null
+      }
+    });
+    return book;
+  }
+
+  async getBookByKeyword (q) {
+    const book = await Book.findOne({
+      where: {
+        title: {
+          [Sequelize.Op.like]: `%${q}%`
+        },
+        delete_time: null
+      }
+    });
+    return book;
+  }
+
+  async getBooks () {
+    const books = await Book.findAll({
+      where: {
+        delete_time: null
+      }
+    });
+    return books;
+  }
+  async createBook (v) {
+    const book = await Book.findOne({
+      where: {
+        title: v.get("title"),
+        delete_time: null
+      }
+    });
+    if (book) {
+      throw new Forbidden({
+        msg: "图书已存在"
+      });
+    }
+    const bk = new Book();
+    bk.title = v.get("title");
+    bk.author = v.get("author");
+    bk.summary = v.get("summary");
+    bk.image = v.get("image");
+    bk.save();
+  }
+
+  async updateBook (v, id) {
+    const book = await Book.findById(id);
+    if (!book) {
+      throw new NotFound({
+        msg: "没有找到相关书籍"
+      });
+    }
+    book.title = v.get("title");
+    book.author = v.get("author");
+    book.summary = v.get("summary");
+    book.image = v.get("image");
+    book.save();
+  }
+
+  async deleteBook (id) {
+    const book = await Book.findOne({
+      where: {
+        id,
+        delete_time: null
+      }
+    });
+    if (!book) {
+      throw new NotFound({
+        msg: "没有找到相关书籍"
+      });
+    }
+    book.softDelete();
+  }
+}
+
+exports.BookDao = BookDao;
