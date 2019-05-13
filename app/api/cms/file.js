040e027afa138253fb56cf29745a921d0ca3b467
pedro <gaopedro@163.com>
2019-05-13 16:03:26 +0800

    feat:添加接口限频

--- app/api/cms/file.js
+++ app/api/cms/file.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const { LinRouter } = require('lin-mizar');
+const { LinRouter, NotFound } = require('lin-mizar');
+const { ratelimit } = require('lin-mizar/lin/limiter');
 const { LocalUploader } = require('../../extensions/file/local-uploader');
+const redis = require('redis');
+
+const client = redis.createClient();
 
 const file = new LinRouter({
   prefix: '/cms/file'
@@ -9,9 +13,28 @@ const file = new LinRouter({
 
 file.post('/', async ctx => {
   const files = await ctx.multipart();
+  if (files.length < 1) {
+    throw new NotFound({ msg: '未找到符合条件的文件资源' });
+  }
   const uploader = new LocalUploader('app/assets');
   await uploader.upload(files);
   ctx.success();
 });
+
+file.get(
+  '/',
+  ratelimit({
+    db: client,
+    duration: 30 * 1000,
+    max: 5,
+    // throw: true,
+    logging: true
+  }),
+  async ctx => {
+    ctx.body = {
+      msg: 'just a normal response.'
+    };
+  }
+);
 
 module.exports = { file };
